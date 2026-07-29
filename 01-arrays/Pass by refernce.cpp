e28a2e33688beadd7fb7b6affa227f57d2d2c18c
Ayush Raj <aysuhraj69@gmail.com>
2026-07-29 13:27:46 +0530

    Complete pass by reference practice

--- 01-arrays/Pass by refernce.cpp
+++ 01-arrays/Pass by refernce.cpp
@@ -1,8 +1,20 @@
-#include <iostrean>
+#include <iostream>
 using namespace std;
 
 void changeArr(int arr[],int size){
+    cout<<"Inside function"<<endl;
     for(int i=0;i<size;i++){
         arr[i] = arr[i]*2;
     }
+} 
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    changeArr(arr,5);
+    cout<<"Inside main function"<<endl;
+    for (int i=0;i<5;i++){
+        cout<<arr[i]<<endl;
+    }
+    cout<<endl;
+    return 0;
 }
